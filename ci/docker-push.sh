409eeb350e6eb74bb863fc9750bf16d8c4829537
thor2304 <49482313+thor2304@users.noreply.github.com>
2023-08-17 13:35:46 +0200

    Container registry (#18)
    * Added Dockerfile and docker-compose file for both front-end and backe-end
    
    * Docker in workflow file (#17)
    
    * Added Dockerfile and docker-compose file for both front-end and backe-end (#13)
    
    * Added docker build and push to workflow
    
    * Changed from our runner to ubuntu-latest for docker
    
    * Update main.yml
    
    * Ls in runner
    
    * chmod docker.push script
    
    * docker push script added and compose file improved
    
    * Added version tag to workflow
    
    * Update main.yml
    
    * Adding env variable for workflow
    
    * Changing the workflow env to use rep owner
    
    * Adding docker username lower case
    
    * Changed image location for the tag
    
    * Added docker image ls to docker build
    
    * Moved the VERSION_TAG env to env section
    
    * Adding write permissions for docker push
    
    * Updated with better bash script usage
    
    * added debug versions of containers using alpine
    
    ---------
    
    Co-authored-by: Kevin Torp <kevintorp@hotmail.com>

---
+++ ci/docker-push.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+echo "$docker_password" | docker login ghcr.io --username "$docker_username" --password-stdin
+
+docker_username_lower=$(echo "$docker_username" | tr '[:upper:]' '[:lower:]')
+docker_username=$docker_username_lower
+
+export backendVersion="ghcr.io/$docker_username/fortune-cookie/backend:$VERSION_TAG" 
+export backendLatest="ghcr.io/$docker_username/fortune-cookie/backend:latest" 
+
+export frontendVersion="ghcr.io/$docker_username/fortune-cookie/frontend:$VERSION_TAG"   
+export frontendLatest="ghcr.io/$docker_username/fortune-cookie/frontend:latest" 
+
+
+docker push $backendVersion
+
+docker tag $backendVersion $backendLatest
+
+docker push $backendLatest
+
+docker push $backendVersion-debug
+# Backend pushed
+
+
+docker push $frontendVersion
+
+docker tag $frontendVersion $frontendLatest
+
+docker push $frontendLatest  
+
+docker push $frontendVersion-debug &
+wait
