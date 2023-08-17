c26b220eeb8c620f68e2ee82e242b8e5279be159
Kevin Torp <Kevintorp@hotmail.com>
2023-08-17 08:34:22 +0000

    Using pwd to find path

--- ci/unit-test.sh
+++ ci/unit-test.sh
@@ -1,10 +1,12 @@
 #! /bin/bash
+pwd
+ls
 #Enters frontend
-cd /frontend/
+cd frontend/
 #Runs tests in frontend
 go test -v
 
 #Enters backend
-cd /backend/
+cd backend/
 #Runs tests in backend
 go test -v
