75db2e2754451dbd96539ec092306db5c030ea9f
thor2304 <49482313+thor2304@users.noreply.github.com>
2023-08-21 14:06:54 +0100

    Improving functional test (#34)
    * Made the deployment wait for functional testing to complete before pushing
    
    * Update main.yml
    
    * Create testing.yaml
    
    #3 
    Created a workflow for the testing that will be used in both dev and main workflows
    
    * Create dev.yaml
    
    Created the development workflow to run on branches that are not main
    
    * Added a pull request workflow
    #3
    
    * changed extensions to conform to .yml
    #3
    
    * Added permissions to testing job
    #3
    
    * Added env variables in the with block
    #3
    
    * Added secrets.docker_password
    
    This should pass with the reuseable workflow, but not sure.
    
    * Changed from secrets.github_token to github.token
    
    * testing.yml now requires all three attributes
    
    * Setting the secrets required to env variables
    
    * REmoving permissions on pullrequest
    
    * Using solution from web
    Link: https://stackoverflow.com/questions/73305126/passing-env-variable-inputs-to-a-reusable-workflow
    
    * Commented unusable call to secrets in dev.yml
    
    * Commented Testing to test if it is invalid
    
    * Added Testing job back without empty space between jobs
    
    * Using github.token instead of secrets.github_token
    
    * Using secrets to pass docker_password instead of with keyword
    
    * Using secrets to pass docker_password instead of with keyword
    
    * Removed old commented cold
    
    * Using with since secrets doesn't work like thought
    
    * added types to inputs
    
    * Accidently echoed Version tag twice instead of docker_password
    
    * Fixing bug regarding docker push
    
    * Passing password with secrets instead of inputs
    
    * Solution is now applied to dev.yml
    
    * added space som yml is correct
    
    * Added if statement to print job
    
    * Test of functional enpoint test
    
    * Retry for the runner
    
    * trying to get the if statement right
    
    * ahh here wo go again
    
    * is pr-check null?
    
    * the needs.pr-check should compare to the raw null not the string null
    
    * Added missing needs for the print job to the docker-build job
    
    * the env is needed with the setup where it is passed to the next steps
    
    * Revert "Test of functional enpoint test"
    
    This reverts commit daf1363166a05145414263163f022251746c152f.
    
    * Improved the output of the python script
    
    * Testing the pipeline with a semi-available container
    
    * Revert "Testing the pipeline with a semi-available container"
    
    This reverts commit caae2edf7662993e3450b36616427681b4641375.
    
    ---------
    
    Co-authored-by: TobiasBak <tobib21@student.sdu.dk>

--- ci/functional-testing.py
+++ ci/functional-testing.py
@@ -1,9 +1,13 @@
+import sys
+
 import requests
 import yaml
 
 
 def get_endpoint(endpoint_name: str) -> str:
     stream = requests.get(f"http://localhost:8080{endpoint_name}")
+    if not stream.ok:
+        raise Exception(f"The endpoint: {endpoint_name} is not available", stream)
     return stream.text
 
 
@@ -17,9 +21,17 @@ def main():
     with open('functional_endpoints.yaml', 'r') as file:
         endpoints = yaml.safe_load(file)
 
-    for endpoint in endpoints["endpoints"]:
-        print_endpoint(endpoint)
+    errors = []
 
+    for endpoint in endpoints["endpoints"]:
+        try:
+            print_endpoint(endpoint)
+        except Exception as e:
+            errors.append(e)
+
+    if len(errors) > 0:
+        print(f"Some endpoints caused errors: {errors}", file=sys.stderr)
+        exit(1)
 
 if __name__ == '__main__':
     main()
